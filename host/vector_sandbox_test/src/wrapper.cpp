f793ae6ba470370b0a9555fe3d5cc547cb201a67
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-08-11 14:41:28 +0800

    vector: add wrapper
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

---
+++ host/vector_sandbox_test/src/wrapper.cpp
@@ -0,0 +1,108 @@
+/**
+* Copyright (C) 2020 Xilinx, Inc
+*
+* Licensed under the Apache License, Version 2.0 (the "License"). You may
+* not use this file except in compliance with the License. A copy of the
+* License is located at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
+* under the License.
+*/
+
+#include <hls_vector.h>
+#include <hls_stream.h>
+#include "assert.h"
+
+#define MAX_DIM 160
+#define PARALLEL 64
+
+// Tripcount identifiers
+const int c_size = MAX_DIM;
+
+extern "C" {
+
+//Function list
+//
+//1
+void madd2(hls::vector<unsigned int, PARALLEL>* c,
+          hls::vector<unsigned int, PARALLEL>* a,
+          hls::vector<unsigned int, PARALLEL>* b,
+	  const int dim0,
+	  const int dim1);
+//2
+void madd3(hls::vector<unsigned int, PARALLEL>* c,
+          hls::vector<unsigned int, PARALLEL>* a,
+          hls::vector<unsigned int, PARALLEL>* b,
+	  const int dim0,
+	  const int dim1);
+//3
+void madd4(hls::vector<unsigned int, PARALLEL>* c,
+          hls::vector<unsigned int, PARALLEL>* a,
+          hls::vector<unsigned int, PARALLEL>* b,
+	  const int dim0,
+	  const int dim1);
+//4
+void mscale2(hls::vector<unsigned int, PARALLEL>* inout_r,
+		const int scale,
+	  const int dim0,
+	  const int dim1);
+//5
+void mscale3(hls::vector<unsigned int, PARALLEL>* inout_r,
+		const int scale,
+	  const int dim0,
+	  const int dim1);
+//6
+void mscale4(hls::vector<unsigned int, PARALLEL>* inout_r,
+		const int scale,
+	  const int dim0,
+	  const int dim1);
+//7
+void mmult2(int* c, int* a, const int* b, const int dim0, const int dim1);
+//8
+void mmult3(int* c, int* a, const int* b, const int dim0, const int dim1);
+//9
+void mmult4(int* c, int* a, const int* b, const int dim0, const int dim1);
+
+/* 
+ * This is a wrapper to dispatch functions,
+ * The first argument is always the function_no,
+ * The remaining arguments are big set of all included functions
+ * */
+void function_handler_wrapper(
+	  int func_no,
+          hls::vector<unsigned int, PARALLEL>* c,
+          hls::vector<unsigned int, PARALLEL>* a,
+          hls::vector<unsigned int, PARALLEL>* b,
+	  const int scale,
+	  const int dim0,
+	  const int dim1
+	  ) 
+{
+	switch (func_no):{
+		case 1:
+			return madd2(c,a,b,dim0,dim1);
+		case 2:
+			return madd3(c,a,b,dim0,dim1);
+		case 3:
+			return madd4(c,a,b,dim0,dim1);
+		case 4:
+			return mscale2(c,scale,dim0,dim1);
+		case 5:
+			return mscale3(c,scale,dim0,dim1);
+		case 6:
+			return mscale4(c,scale,dim0,dim1);
+		case 7:
+			return mmult2(c,a,b,dim0,dim1);
+		case 8:
+			return mmult3(c,a,b,dim0,dim1);
+		case 9:
+			return mmult4(c,a,b,dim0,dim1);
+	}
+}
+
+}
